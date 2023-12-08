08fdabd9ce4b0476d5c0b92325bc1e64363dff2b
aurghya-0 <aurghya.blog@gmail.com>
2023-12-08 09:36:35 +0530

    added todo

--- whisper_transcribe.py
+++ whisper_transcribe.py
@@ -5,6 +5,11 @@ client = OpenAI(
     api_key="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",
 )
 
+## TODO
+  # 1. Get PyDub to break down the audio file into 10 minute chunks
+  # 2. Get the transcript for each chunk
+  # 3. Write the transcript to a file
+
 # get the recorded audio file
 audio_file= open("output.wav", "rb")
 
@@ -15,6 +20,7 @@ transcript = client.audio.transcriptions.create(
   response_format="text"
 )
 
+# write the transcript as file
 with open("transcript.txt", "w") as file:
     file.write(transcript)
 
